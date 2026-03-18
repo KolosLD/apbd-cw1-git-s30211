b2d82b1144bb27ca2a6e1528c06284a018368c33
KolosLD <s30211@pjwstk.edu.pl>
2026-03-18 10:47:19 +0100

    Add average method

--- Calculator/src/StatisticsHelper.java
+++ Calculator/src/StatisticsHelper.java
@@ -1,2 +1,8 @@
 public class StatisticsHelper {
+    public static double calculateAverage(int[] values) {
+        if (values.length == 0) return 0;
+        int sum = 0;
+        for (int v : values) sum += v;
+        return (double) sum / values.length;
+    }
 }
