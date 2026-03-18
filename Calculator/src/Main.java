d47ec8e876531951455b8bb70db60fdb28f2cec5
KolosLD <s30211@pjwstk.edu.pl>
2026-03-18 10:57:18 +0100

    Add division with zero check

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -21,4 +21,12 @@ public class Main {
     public static int multiply(int a, int b) {
         return a * b;
     }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero!");
+            return 0;
+        }
+        return (double) a / b;
+    }
 }
